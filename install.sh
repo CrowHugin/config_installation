33448cb90d03bd859d776fbbbd2ad16acfc64b6c
CrowHugin <kuntz.bastien@gmail.com>
2026-02-01 14:11:14 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+
+
+apt-get install software-properties-common
+add-apt-repository ppa:neovim-ppa/unstable
+apt update 
+apt-get install neovim
+
+git clone https://github.com/CrowHugin/nvim-config ~/.config/nvim
