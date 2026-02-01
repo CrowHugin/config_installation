e7a3b019a9ae73702ca834263be22a63865d0440
CrowHugin <kuntz.bastien@gmail.com>
2026-02-01 15:06:38 +0100

    Add -y to download option

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 
 
 
-apt-get install software-properties-common
-add-apt-repository ppa:neovim-ppa/unstable
+apt-get install software-properties-common -y
+add-apt-repository ppa:neovim-ppa/unstable -y
 apt update 
-apt-get install neovim
+apt-get install neovim -y
+
+apt-get install unzip -y
+apt-get install npm -y
+
+
 
 git clone https://github.com/CrowHugin/nvim-config ~/.config/nvim
