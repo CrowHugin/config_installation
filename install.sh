24bcf690813be5d1ebb326102611eadd53473344
CrowHugin <kuntz.bastien@gmail.com>
2026-02-01 16:09:47 +0100

    Removed sudo need

--- install.sh
+++ install.sh
@@ -9,21 +9,17 @@ set -e
 
 echo "Installing CrowHugin's config..."
 
-#Ensure the script is launch as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please be sure to be root."
-  exit 1
-fi
 
 
 echo "Installing ..."
-apt-get install software-properties-common -y
-add-apt-repository ppa:neovim-ppa/unstable -y
-apt update 
-apt-get install neovim -y
 
-apt-get install unzip -y
-apt-get install npm -y
+sudo apt update 
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt-get install neovim -y
+
+sudo apt-get install unzip -y
+sudo apt-get install npm -y
 
 if [-d "$NVIM_CONFIG"]; then
   echo "An nvim config was detected, will be move to $NVIM_CONFIG.bak"
